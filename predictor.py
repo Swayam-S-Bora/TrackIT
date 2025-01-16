d859c55303f515732259f7d2867c6bcce50e2bfe
Swayam Singh Bora <swayam.bora.309@gmail.com>
2025-01-16 16:45:00 +0530

    Integrate prediction model into app.py

--- predictor.py
+++ predictor.py
@@ -120,3 +120,4 @@ class PricePredictionModel:
 # Commit on 2025-01-14T19:29:00+05:30
 # Commit on 2025-01-14T18:11:00+05:30
 # Commit on 2025-01-16T19:00:00+05:30
+# Commit on 2025-01-16T16:45:00+05:30
