adb544252ff2e449f967bc557e97b636d1597079
Swayam Singh Bora <swayam.bora.309@gmail.com>
2025-02-06 11:54:00 +0530

    Implement data cleaning and preprocessing in predictor

--- predictor.py
+++ predictor.py
@@ -129,3 +129,4 @@ class PricePredictionModel:
 # Commit on 2025-01-27T19:26:00+05:30
 # Commit on 2025-01-30T11:24:00+05:30
 # Commit on 2025-02-06T18:00:00+05:30
+# Commit on 2025-02-06T11:54:00+05:30
