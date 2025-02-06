8a0d720eb00843ec0f0d45f223bcb4d494cb43ff
Swayam Singh Bora <swayam.bora.309@gmail.com>
2025-02-06 18:00:00 +0530

    Create predictor.py with PricePredictionModel class

--- predictor.py
+++ predictor.py
@@ -128,3 +128,4 @@ class PricePredictionModel:
 # Commit on 2025-01-27T12:08:00+05:30
 # Commit on 2025-01-27T19:26:00+05:30
 # Commit on 2025-01-30T11:24:00+05:30
+# Commit on 2025-02-06T18:00:00+05:30
