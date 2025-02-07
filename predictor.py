e370853d63b20e6983fd97d882bfad452630adf2
Swayam Singh Bora <swayam.bora.309@gmail.com>
2025-02-07 09:58:00 +0530

    Add feature engineering (avg_price, price_std)

--- predictor.py
+++ predictor.py
@@ -130,3 +130,4 @@ class PricePredictionModel:
 # Commit on 2025-01-30T11:24:00+05:30
 # Commit on 2025-02-06T18:00:00+05:30
 # Commit on 2025-02-06T11:54:00+05:30
+# Commit on 2025-02-07T09:58:00+05:30
